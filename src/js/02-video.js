633b7071f42aa70076299decd4feb270264315a2
Rost Redvanskyi <redvanskyirost@gmail.com>
2022-12-05 21:04:59 +0200

    fix errors task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,12 @@ const player = new Player(iframe);
 const LOCAL_KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
-player.setCurrentTime(localStorage.getItem(LOCAL_KEY));
+
 function onTimeUpdate(data) {
   localStorage.setItem(LOCAL_KEY, data.seconds);
+}
+
+const lastCurrentTime = localStorage.getItem(LOCAL_KEY);
+if (lastCurrentTime) {
+  player.setCurrentTime(localStorage.getItem(LOCAL_KEY));
 }
